ad94d5bbf161c806677c9af9508809e2deffad87
Yuki88ab <marinieves1988@gmail.com>
2024-07-03 12:57:53 +0200

    hola

--- Sala_de_Juegos/src/main/java/com/mycompany/sala_de_juegos/PrimaryController.java
+++ Sala_de_Juegos/src/main/java/com/mycompany/sala_de_juegos/PrimaryController.java
@@ -1,12 +1,52 @@
-package com.mycompany.sala_de_juegos;
+package com.mycompany.mavenproject1;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
 
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PrimaryController {
 
+
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button tecla1;
+
+    @FXML
+    private Button tecla2;
+
+    @FXML
+    private Button tecla3;
+
+    @FXML
+    private Button tecla4;
+
+    @FXML
+    private Button tecla5;
+
+    @FXML
+    private Button tecla6;
+
+    @FXML
+    private Button tecla7;
+
+    @FXML
+    private Button tecla8;
+
+    @FXML
+    void initialize() {
+       
+
     }
+
 }
